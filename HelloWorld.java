94057dfcb7db6dc8db57a6b5b516757d0fa6c26f
BrooksJoshua <51010834+BrooksJoshua@users.noreply.github.com>
2019-09-01 15:00:05 +0800

    修改算法
    加法改成了乘法

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld{
   public static void main(String[] args){
       int a =1;
       int b=2;
-      System.out.println("a+b=3");
+      System.out.println("a*b=2");
     } 
 }
